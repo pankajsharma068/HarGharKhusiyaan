5589fc38e77d6613e2ff0ba4b98ae99679f8674b
pankajsharma068 <77068519+pankajsharma068@users.noreply.github.com>
2026-01-30 03:48:25 +0530

    small gallery change

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -32,54 +32,48 @@ export class GalleryComponent {
     },
     {
       id: 3,
-      url: 'https://drive.google.com/thumbnail?id=1fnhLJMcKUZ2nGCEQJD7lue903cT4nGyM&sz=w1000',
-      title: 'Community Outreach',
-      category: 'community'
-    },
-    {
-      id: 4,
       url: 'https://drive.google.com/thumbnail?id=1k7O_baoIm8TvjG0Cuvqn5yaDpApnLiiu&sz=w1000',
       title: 'Volunteer Activities',
       category: 'volunteers'
     },
     {
-      id: 5,
+      id: 4,
       url: 'https://drive.google.com/thumbnail?id=1u7zo3OsBWuMWusni5XVmAq-IALNoAe9-&sz=w1000',
       title: 'Community Initiative',
       category: 'community'
     },
     {
-      id: 6,
+      id: 5,
       url: 'https://drive.google.com/thumbnail?id=1nu5XzPX4Tk8ryBBK9VawEou86OocKH6k&sz=w1000',
       title: 'Social Programs',
       category: 'community'
     },
     {
-      id: 7,
+      id: 6,
       url: 'https://drive.google.com/thumbnail?id=1TDo2xDxj9mo0wEZRe8uWsdtG2qFo-D2i&sz=w1000',
       title: 'Fundraising Event',
       category: 'events'
     },
     {
-      id: 8,
+      id: 7,
       url: 'https://drive.google.com/thumbnail?id=1IDPJQK2YJASc8GPgSt_1REx0RZRyrAmb&sz=w1000',
       title: 'Community Support',
       category: 'community'
     },
     {
-      id: 9,
+      id: 8,
       url: 'https://drive.google.com/thumbnail?id=1yYK_8xHHW8NgphwG6SGxgYPAWAtCEjBM&sz=w1000',
       title: 'Tree Plantation Drive',
       category: 'environment'
     },
     {
-      id: 10,
+      id: 9,
       url: 'https://drive.google.com/thumbnail?id=1myHdXfHk2qOYi26dbo8sr3nDYxDzY6hN&sz=w1000',
       title: 'Green Initiative',
       category: 'environment'
     },
     {
-      id: 11,
+      id: 10,
       url: 'https://drive.google.com/thumbnail?id=1JZ3wHbf3yt_9C_ty8nJTLJx43ZZMhVse&sz=w1000',
       title: 'Environmental Campaign',
       category: 'environment'
